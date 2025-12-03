d79d09bea0b28133c8b0d3e857055046c0c2eea6
wesley <340308@student.mboutrecht.nl>
2025-12-03 12:48:22 +0100

    navbar update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -46,10 +46,9 @@
                             {{ _('Berichten') }}
                         </x-nav-link>
                     </div>
-    
                 @endif
 
-                
+
                 @if (Auth::check() && in_array(Auth::user()->RolNaam, ['Praktijkmanagement']))
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                         <x-nav-link :href="route('medewerkers.overzicht')" :active="request()->routeIs('medewerkers.overzicht')">
@@ -87,7 +86,8 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -101,10 +101,10 @@
                 <button @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
@@ -113,12 +113,53 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+
+        @if (Auth::check() && in_array(Auth::user()->RolNaam, ['Patient']))
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('facturenOverzichtPatient.index')" :active="request()->routeIs('facturenOverzichtPatient.index')">
+                    {{ _('Mijn facturen') }}
+                </x-responsive-nav-link>
+            </div>
+        @endif
+
+        @if (Auth::check() && in_array(Auth::user()->RolNaam, ['Praktijkmanagement', 'Tandarts', 'Assistent', 'mondhygienist']))
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('overzicht-patienten.index')" :active="request()->routeIs('overzicht-patienten.index')">
+                    {{ _('patienten-overzicht') }}
+                </x-responsive-nav-link>
+            </div>
+        @endif
+
+
+        @if (Auth::check() && in_array(Auth::user()->RolNaam, ['Praktijkmanagement']))
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('praktijkmanagement.index')" :active="request()->routeIs('praktijkmanagement.index')">
+                    {{ _('Praktijkmanagement') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('praktijkmanagement.berichten')" :active="request()->routeIs('praktijkmanagement.berichten')">
+                    {{ _('Berichten') }}
+                </x-responsive-nav-link>
+            </div>
+        @endif
+
+        @if (Auth::check() && in_array(Auth::user()->RolNaam, ['Praktijkmanagement']))
+                    <div class="pt-2 pb-3 space-y-1">
+                        <x-responsive-nav-link :href="route('medewerkers.overzicht')" :active="request()->routeIs('medewerkers.overzicht')">
+                            {{ _('Medewerkers') }}
+                        </x-responsive-nav-link>
+                    </div>
+                @endif
+
+
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
@@ -136,7 +177,8 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')"
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
